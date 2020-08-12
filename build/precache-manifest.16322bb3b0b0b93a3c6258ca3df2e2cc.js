e3bb3c3613dcac67f1ce15ebb55e52bb5a16ecbf
DIEU <dvdieukhtn@gmail.com>
2020-08-13 01:37:33 +0700

    update

---
+++ build/precache-manifest.16322bb3b0b0b93a3c6258ca3df2e2cc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bda448cb2c8563c7b359d9c557b47ca1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f2d196f3c9fd5b9c0a41",
+    "url": "/static/css/2.96985c97.chunk.css"
+  },
+  {
+    "revision": "192d7764cb2b5b1781ef",
+    "url": "/static/css/main.06733824.chunk.css"
+  },
+  {
+    "revision": "f2d196f3c9fd5b9c0a41",
+    "url": "/static/js/2.c76be045.chunk.js"
+  },
+  {
+    "revision": "0e0fff8f2811560c6af577c28fc3302e",
+    "url": "/static/js/2.c76be045.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "192d7764cb2b5b1781ef",
+    "url": "/static/js/main.0933cb28.chunk.js"
+  },
+  {
+    "revision": "d25bc4a59227f20584f5",
+    "url": "/static/js/runtime-main.2a5a7654.js"
+  },
+  {
+    "revision": "cb4eae95434cd0c2a1e34aca255b52dc",
+    "url": "/static/media/avatar.cb4eae95.png"
+  }
+]);
