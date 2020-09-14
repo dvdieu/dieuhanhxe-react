35a620d59e53f341e610324b024088a40748e8af
DIEU <dvdieukhtn@gmail.com>
2020-09-14 15:13:16 +0700

    update domai

--- src/config/domains.js
+++ src/config/domains.js
@@ -1,9 +1,9 @@
-export const base_url = 'http://localhost:8080/api';
+export const base_url = 'http://45.117.168.135/api';
 
-export const warehouse_url = 'http://localhost:8080/api';
+export const warehouse_url = 'http://45.117.168.135/api';
 
-export const order_url = 'http://localhost:8080/api';
+export const order_url = 'http://45.117.168.135/api';
 
-export const direction_url = 'http://localhost:8080/api';
+export const direction_url = 'http://45.117.168.135/api';
 
-export const truck_url = 'http://localhost:8080/api';
+export const truck_url = 'http://45.117.168.135/api';
